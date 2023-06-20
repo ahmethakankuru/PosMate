8aa18a2a820af7bf81b030087916a40ca0a841a9
Ahmet Hakan Kuru <112398802+ahmethakankuru@users.noreply.github.com>
2023-06-20 23:33:35 +0300

    Point of Sales
    Point of Sales .NET

---
+++ PointOfSale/wwwroot/js/views/ReportsSalesReport.js
@@ -0,0 +1,53 @@
+﻿let tableData;
+
+$(document).ready(function () {
+
+    $("#txtStartDate").datepicker({ dateFormat: 'dd/mm/yy' });
+    $("#txtEndDate").datepicker({ dateFormat: 'dd/mm/yy' });
+
+    tableData = $('#tbdata').DataTable({
+        "processing": true,
+        "ajax": {
+            "url": "/Reports/ReportSale?startDate=01/01/1991&endDate=01/01/1991",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "registrationDate" },
+            { "data": "saleNumber" },
+            { "data": "documentType" },
+            { "data": "documentClient" },
+            { "data": "clientName" },
+            { "data": "subTotalSale" },
+            { "data": "taxTotalSale" },
+            { "data": "totalSale" },
+            { "data": "product" },
+            { "data": "quantity" },
+            { "data": "price" },
+            { "data": "total" }
+        ],
+        order: [[1, "desc"]],
+        "scrollX": true,
+        dom: "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+        buttons: [
+            'pageLength'
+        ]
+    });
+
+})
+
+
+
+$("#btnSearch").click(function () {
+
+    if ($("#txtStartDate").val().trim() == "" || $("#txtEndDate").val().trim() == "") {
+        toastr.warning("", "You must enter start and end date");
+        return;
+    }
+
+    var new_url = `/Reports/ReportSale?startDate=${$("#txtStartDate").val().trim()}&endDate=${$("#txtEndDate").val().trim()}`
+
+    tableData.ajax.url(new_url).load();
+})
